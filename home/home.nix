a24798c2257c425ea9cc1d91642868dcc03357aa
Chan Wen Xu <lutherchanpublic+git@gmail.com>
2021-09-11 21:27:37 +0800

    feat: Add kitty

--- home/home.nix
+++ home/home.nix
@@ -1,6 +1,14 @@
 { self, pkgs, ... }:
 
 {
+	programs.kitty = {
+		enable = true;
+		font = {
+			name = "JetBrains Mono";
+			size = 12;
+		};
+	};
+
 	services.polybar = {
 		enable = true;
 		script = "polybar bar &";
