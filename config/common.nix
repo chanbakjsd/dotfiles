3fe0621abdcbc8844cb292009e39ee5b7cc01573
Chan Wen Xu <lutherchanpublic+git@gmail.com>
2021-12-03 13:05:15 +0800

    config: Switch to pipewire
    An attempt at solving the audio issues I have, proven unfruitful but it
    is a good change regardless.

--- config/common.nix
+++ config/common.nix
@@ -36,7 +36,15 @@
 	];
 
 	sound.enable = true;
-	hardware.pulseaudio.enable = true;
+	hardware.pulseaudio.enable = lib.mkForce false;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		jack.enable = true;
+		pulse.enable = true;
+
+		alsa.support32Bit = true;
+	};
 
 	time.timeZone = "Asia/Kuala_Lumpur";
 
