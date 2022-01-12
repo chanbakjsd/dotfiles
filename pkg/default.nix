4e5646cf18493635c901924edf686e59cb4ae1f8
Chan Wen Xu <lutherchanpublic@gmail.com>
2022-01-12 10:29:04 +0800

    pkg: Bump discord-canary

--- pkg/default.nix
+++ pkg/default.nix
@@ -1,10 +1,19 @@
 pkgs: 
 
 let
+	discord-version = "0.0.132";
+	discord-hash = "1jjbd9qllgcdpnfxg5alxpwl050vzg13rh17n638wha0vv4mjhyv";
 	node-coc-spell-checker = import ./coc-spell-checker { inherit pkgs; };
 	node-coc-svelte = import ./coc-svelte { inherit pkgs; };
 in
 {
+	discord-canary = pkgs.discord-canary.overrideAttrs (ori: {
+		version = "0.0.131";
+		src = builtins.fetchurl {
+			url = "https://dl-canary.discordapp.net/apps/linux/${discord-version}/discord-canary-${discord-version}.tar.gz";
+			sha256 = discord-hash;
+		};
+	});
 	os-prober = pkgs.os-prober.overrideAttrs (ori: {
 		patches = [ ./os-prober.patch ]; # OS Prober is awfully slow in detecting Linux distros.
 	});
